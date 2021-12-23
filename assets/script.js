11a34e7e029de29b7881e40b18a2d93dce403117
KuperIvan <ivankupryianau@gmail.com>
2021-12-23 20:25:04 +0300

    feat: add footer style

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+// let listItems = document.querySelectorAll('.header_link');
+// console.log(listItems);
+
+// document.addEventListener('mouseenter', function() {
+//   listItems.forEach(link => {
+//     link.classList.add('header_link-active')
+//   })
+// });
