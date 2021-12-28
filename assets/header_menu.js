4935e547856209e0df70e6a30badb50e47f89c3a
KuperIvan <ivankupryianau@gmail.com>
2021-12-28 16:28:52 +0300

    feat: add change section by click

--- assets/header_menu.js
+++ assets/header_menu.js
@@ -6,19 +6,22 @@ const header_link_5 = document.getElementById('header_link-5');
 const header_link_6 = document.getElementById('header_link-6');
 const header_link_7 = document.getElementById('header_link-7');
 
-const section_contacts_1 = document.getElementById('contacts');
-const section_contacts_2 = document.getElementById('skills');
-const section_contacts_3= document.getElementById('code');
-const section_contacts_4 = document.getElementById('courses');
-const section_contacts_5 = document.getElementById('projects');
-const section_contacts_6 = document.getElementById('education');
-const section_contacts_7 = document.getElementById('english'); 
+const section_contacts = document.getElementById('contacts');
+const section_skills = document.getElementById('skills');
+const section_code = document.getElementById('code');
+const section_courses = document.getElementById('courses');
+const section_projects = document.getElementById('projects');
+const section_education = document.getElementById('education');
+const section_english = document.getElementById('english');
 
 header_link_1.addEventListener('click', () => {
   if(header_link_1.classList.contains('header_link-active')) {
     header_link_1.classList.remove('header_link-active');
+    section_contacts.classList.remove('section-contacts-active')
     } else {
     header_link_1.classList.add('header_link-active');
+    section_contacts.classList.add('section-contacts-active')
+
     header_link_2.classList.remove('header_link-active');
     header_link_3.classList.remove('header_link-active');
     header_link_4.classList.remove('header_link-active');
@@ -31,8 +34,18 @@ header_link_1.addEventListener('click', () => {
 header_link_2.addEventListener('click', () => {
   if(header_link_2.classList.contains('header_link-active')) {
     header_link_2.classList.remove('header_link-active')
+    section_skills.classList.remove('section-skills-active');
   } else {
     header_link_2.classList.add('header_link-active');
+    section_skills.classList.add('section-skills-active');
+
+    section_contacts.classList.remove('section-contacts-active');
+    section_code.classList.remove('section-code-active');
+    section_courses.classList.remove('section-courses-active');
+    section_projects.classList.remove('section-projects-active');
+    section_education.classList.remove('section-education-active');
+    section_english.classList.remove('section-english-active');
+
     header_link_1.classList.remove('header_link-active');
     header_link_3.classList.remove('header_link-active');
     header_link_4.classList.remove('header_link-active');
@@ -47,6 +60,8 @@ header_link_3.addEventListener('click', () => {
     header_link_3.classList.remove('header_link-active')
   } else {
     header_link_3.classList.add('header_link-active');
+    section_code.classList.add('section-code-active')
+
     header_link_2.classList.remove('header_link-active');
     header_link_1.classList.remove('header_link-active');
     header_link_4.classList.remove('header_link-active');
@@ -62,6 +77,8 @@ header_link_4.addEventListener('click', () => {
     header_link_4.classList.remove('header_link-active')
   } else {
     header_link_4.classList.add('header_link-active')
+    section_courses.classList.add('section-courses-active');
+
     header_link_3.classList.remove('header_link-active');
     header_link_2.classList.remove('header_link-active');
     header_link_1.classList.remove('header_link-active');
@@ -76,6 +93,8 @@ header_link_5.addEventListener('click', () => {
     header_link_5.classList.remove('header_link-active')
   } else {
     header_link_5.classList.add('header_link-active');
+    section_projects.classList.add('section-projects-active');
+
     header_link_4.classList.remove('header_link-active');
     header_link_3.classList.remove('header_link-active');
     header_link_2.classList.remove('header_link-active');
@@ -90,6 +109,8 @@ header_link_6.addEventListener('click', () => {
     header_link_6.classList.remove('header_link-active')
   } else {
     header_link_6.classList.add('header_link-active')
+    section_education.classList.add('section-education-active');
+    
     header_link_5.classList.remove('header_link-active');
     header_link_4.classList.remove('header_link-active');
     header_link_3.classList.remove('header_link-active');
@@ -104,6 +125,9 @@ header_link_7.addEventListener('click', () => {
     header_link_7.classList.remove('header_link-active')
   } else {
     header_link_7.classList.add('header_link-active');
+    section_english.classList.add('section-english-active');
+    
+
     header_link_6.classList.remove('header_link-active')
     header_link_5.classList.remove('header_link-active');
     header_link_4.classList.remove('header_link-active');
---
+++ assets/section.js
@@ -0,0 +1,30 @@
+// header_link_1 = document.getElementById('header_link-1');
+// header_link_2 = document.getElementById('header_link-2');
+// header_link_3 = document.getElementById('header_link-3');
+// header_link_4 = document.getElementById('header_link-4');
+// header_link_5 = document.getElementById('header_link-5');
+// header_link_6 = document.getElementById('header_link-6');
+// header_link_7 = document.getElementById('header_link-7');
+
+
+
+// header_link_1.addEventListener('click', () => {
+//   if (header_link_1.classList.contains('header_link-active')) {
+//    console.log(section_contacts);
+//   }
+// })
+
+// header_link_2.addEventListener('click', () => {
+//   if (header_link_2.classList.contains('header_link-active')) {
+//     section_skills.classList.add('section-skills-active');
+
+//   }
+// })
+
+// .section-contacts-active (flex)
+// .section-skills-active (flex)
+// .section-code-active (grid)
+// .section-courses-active (block)
+// .section-projects-active (flex)
+// .section-education-active (flex)
+// .section-english-active (flex)
